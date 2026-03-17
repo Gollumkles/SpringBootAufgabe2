f59d75bd263389d834ffbaec080529aa87ea42ff
Gollumkles <julian.gantschacher@gmail.com>
2026-03-17 21:19:05 +0100

    Aufgabe teil 3 und 4 loeschen und bearbeiten

--- StudentenverwaltungRestAPIBasisFrontends/src/main/java/at/itkollegimst/studentenverwaltung/controller/StudentThymeleafController.java
+++ StudentenverwaltungRestAPIBasisFrontends/src/main/java/at/itkollegimst/studentenverwaltung/controller/StudentThymeleafController.java
@@ -1,11 +1,14 @@
 package at.itkollegimst.studentenverwaltung.controller;
 
 import at.itkollegimst.studentenverwaltung.domain.Student;
+import at.itkollegimst.studentenverwaltung.exceptions.StudentNichtGefunden;
 import at.itkollegimst.studentenverwaltung.services.StudentenService;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
@@ -39,6 +42,41 @@ public class StudentThymeleafController {
             return "studenteneinfuegen";
         }else{
             this.StudentenService.studentEinfuegen(student);
+            return "redirect:/web/v1/studenten";
+        }
+    }
+
+    @GetMapping("/update/{id}")
+    public String studentenUpdatenFormular(@PathVariable Long id, Model model) {
+        try {
+            Student student = this.StudentenService.studentMitId(id);
+            model.addAttribute("student", student);
+            return "studentenupdaten";
+        } catch (StudentNichtGefunden studentNichtGefunden) {
+            return "redirect:/web/v1/studenten";
+        }
+    }
+
+    @PostMapping("/update")
+    public String studentUpdaten(@Valid Student student, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "studentenupdaten";
+        } else {
+            try {
+                this.StudentenService.studentUpdaten(student);
+                return "redirect:/web/v1/studenten";
+            } catch (StudentNichtGefunden studentNichtGefunden) {
+                return "redirect:/web/v1/studenten";
+            }
+        }
+    }
+
+    @GetMapping("/delete/{id}")
+    public String studentLoeschen(@PathVariable Long id) {
+        try {
+            this.StudentenService.studentLoeschenMitId(id);
+            return "redirect:/web/v1/studenten";
+        } catch (StudentNichtGefunden studentNichtGefunden) {
             return "redirect:/web/v1/studenten";
         }
     }
